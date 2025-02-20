8f1bc7a9633f845348c50f89b95af7dc80bd9370
Joseph Washum <Jkwashu@gmail.com>
2025-02-20 01:35:39 -0600

    Major changes, font implemented, game over screen, healthbar, etc.

--- CoffeeCraze/Assets/CoffeeShop.cs
+++ CoffeeCraze/Assets/CoffeeShop.cs
@@ -1,28 +1,73 @@
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class CoffeeShop : MonoBehaviour
 {
+    [SerializeField] private GameOverManager gameOverManager;
+    [Header("Health Settings")]
     [SerializeField] private int maxHealth = 100;
+    [SerializeField] private RectTransform healthBarFill;
+    
+    [Header("Effects")]
+    [SerializeField] private ParticleSystem damageEffect;
+    [SerializeField] private AudioClip damageSound;
+    
     private int currentHealth;
+    private float initialHealthBarWidth;
+    private AudioSource audioSource;
     
     public UnityEvent<int> onHealthChanged;
     public UnityEvent onGameOver;
 
-    void Start()
+    private void Start()
     {
         currentHealth = maxHealth;
-        onHealthChanged?.Invoke(currentHealth);
+        audioSource = GetComponent<AudioSource>();
+        if (healthBarFill != null)
+        {
+            initialHealthBarWidth = healthBarFill.sizeDelta.x;
+        }
+        UpdateHealthBar();
     }
 
     public void TakeDamage(int damage)
     {
         currentHealth = Mathf.Max(0, currentHealth - damage);
-        onHealthChanged?.Invoke(currentHealth);
+        UpdateHealthBar();
+        
+        // Play effects
+        if (damageEffect != null)
+        {
+            damageEffect.Play();
+        }
+        
+        if (damageSound != null && audioSource != null)
+        {
+            audioSource.PlayOneShot(damageSound);
+        }
         
         if (currentHealth <= 0)
         {
-            onGameOver?.Invoke();
+            GameOver();
+        }
+    }
+
+    private void UpdateHealthBar()
+    {
+        if (healthBarFill != null)
+        {
+            float healthPercentage = (float)currentHealth / maxHealth;
+            healthBarFill.localScale = new Vector3(healthPercentage, 1, 1);
+        }
+    }
+
+    private void GameOver()
+    {
+        onGameOver?.Invoke();
+        if (gameOverManager != null)
+        {
+            gameOverManager.ShowGameOver();
         }
     }
 }
